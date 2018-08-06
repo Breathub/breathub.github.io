debec775b774390e6f2cf7402f3fafd5da7c4624
Bahadir Can Yildiz <bahadircyildiz@yandex.com>
2018-08-06 17:22:41 +0300

    Font-awesome used as node package

--- gulpfile.js
+++ gulpfile.js
@@ -60,17 +60,9 @@ gulp.task('copy', function() {
 
     gulp.src(['node_modules/simple-line-icons/*/*'])
         .pipe(gulp.dest('vendor/simple-line-icons'))
-
-
-    gulp.src([
-            'node_modules/font-awesome/**',
-            '!node_modules/font-awesome/**/*.map',
-            '!node_modules/font-awesome/.npmignore',
-            '!node_modules/font-awesome/*.txt',
-            '!node_modules/font-awesome/*.md',
-            '!node_modules/font-awesome/*.json'
-        ])
-        .pipe(gulp.dest('vendor/font-awesome'))
+        
+    gulp.src('node_modules/font-awesome/fonts/*')
+        .pipe(gulp.dest('fonts'))
 })
 
 // Run everything
